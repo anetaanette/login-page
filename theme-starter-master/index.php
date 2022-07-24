905676baab16e7c7b21e7ea0f71dc83cec7d167e
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-07-24 18:14:38 +0200

    mobile ready

--- theme-starter-master/index.php
+++ theme-starter-master/index.php
@@ -2,11 +2,26 @@
 get_header();
 ?>
 
-<?php the_content(); ?>
+	<body>
+	<main>
+		<h1>Log in</h1>
+		<p>Log in with your data that you received from the photographer</p>
 
-<?php
-echo esc_html( get_template_part( 'template-parts/pagination', 'infinite-scroll' ) );
-?>
+		<section class="flex flex-inputs">
+			<label>
+				<input type="text" name="email" class="input-mail" placeholder="E-mail">
+			</label>
+			<label>
+				<input type="text" name="password" class="input-password" placeholder="Password">
+			</label>
+		</section>
+
+		<section class="flex flex-inline">
+			<a href="#">Forgot password?</a>
+			<button class="button">Log in</button>
+		</section>
+	</main>
+	</body>
 
 <?php
 get_footer();
