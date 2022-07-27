81bd72f6103c1d5834442a94091dbafc7b9f53a5
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-07-27 10:06:05 +0200

    BEM done

--- theme-starter-master/index.php
+++ theme-starter-master/index.php
@@ -3,17 +3,17 @@ get_header();
 ?>
 
 	<section class="form">
-		<h1 class="form_title">Log in</h1>
-		<p class="form_paragraph">Log in with your data that you received from the photographer</p>
+		<h1 class="form__title">Log in</h1>
+		<p class="form__paragraph">Log in with your data that you received from the photographer</p>
 
-		<form class="form_inputs">
-				<input type="text" name="email" class="form_inputs-input inputs-mail" placeholder="E-mail">
-				<input type="text" name="password" class="form_inputs-input inputs-password" placeholder="Password">
+		<form class="form__inputs">
+				<input type="text" name="email" class="form__inputs-input form__inputs-mail" placeholder="E-mail">
+				<input type="text" name="password" class="form__inputs-input form__inputs-password" placeholder="Password">
 		</form>
 
-		<div class="form_inline">
-			<a href="#" class="form_inline-reminder">Forgot password?</a>
-			<button class="form_inline-button">Log in</button>
+		<div class="form__inline">
+			<a href="#" class="form__inline-reminder">Forgot password?</a>
+			<button class="form__inline-button">Log in</button>
 		</div>
 	</section>
 
